de2a1be7c85d9474f49b638e3aab811b56bcd3a0
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-01-03 14:47:03 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        int a = System.in.read()-48;
+        int z = System.in.read();
+        int b = System.in.read()-48;
+
+        System.out.println((double)a/b);
+    }
+}
