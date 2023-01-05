0b84212cb32013b95f3464bd1e85b82bb6c7e354
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-01-05 12:27:37 +0900

    [Bronze V] Title: 팩토리얼, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = 1;
+        
+        if(num != 0){
+            for(int i=1; i<=num; i++){
+                result *= i;
+            }
+        }
+        else{
+            result = 1;
+        }
+        
+        System.out.println(result);
+        
+        
+    }
+}
