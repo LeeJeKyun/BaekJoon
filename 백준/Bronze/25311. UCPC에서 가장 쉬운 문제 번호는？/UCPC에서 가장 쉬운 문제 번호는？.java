5e1675376967bd1438438248d0c0197ad5028285
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-01-10 12:10:21 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year == 2018){
+            System.out.println("A");
+        } else if(year == 2019){
+            System.out.println("A");
+        } else if(year == 2020){
+            System.out.println("A");
+        } else if(year == 2021){
+            System.out.println("A");
+        } else if(year ==2022) {
+            System.out.println("A");
+        }
+        
+        
+    }
+}
