dfb3f6895c63a13a83a6292c9e71c721ecc36f4f
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-01-12 10:44:53 +0900

    [Bronze II] Title: 단어의 개수, Time: 476 ms, Memory: 30916 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -8,6 +8,8 @@ class Main{
         String str = sc.nextLine();
         StringTokenizer token = new StringTokenizer(str, " ");
         System.out.println(token.countTokens());
+        /*String.split으로 접근했었으나 예제 입력 2번의 앞에 공백으로 인해 단어의 개수보다 1개가 늘어나서
+        접근이 되었다. StringKokenizer를 활용하는 것이 문제의 의도인듯 하다*/
         
     }
 }
