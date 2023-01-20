cce225bb2738aab7b2a728b48496e1042edd06ad
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-01-20 17:24:16 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 1.36 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] sArr = s.split("");
+        if(sArr.length%2==0){
+            answer = sArr[sArr.length/2-1];
+            answer += sArr[sArr.length/2];
+        } else{
+            answer = sArr[sArr.length/2];
+        }
+        
+        
+        return answer;
+    }
+}
