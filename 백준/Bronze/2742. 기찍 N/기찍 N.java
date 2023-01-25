ae6bdce9cae815f92b042e9d972c35b7efccff16
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-01-25 09:03:58 +0900

    [Bronze IV] Title: 기찍 N, Time: 1124 ms, Memory: 32932 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        for(int i=sc.nextInt(); i>0; i--){
+            System.out.println(i);
+        }
+        
+    }
+}
