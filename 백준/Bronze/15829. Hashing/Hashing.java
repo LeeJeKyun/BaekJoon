f727d0baae166019f9111d8ae1d23470824e6bae
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2023-03-17 15:37:37 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,24 +1,37 @@
 import java.util.Scanner;
+import java.lang.Math;
+import java.math.BigInteger;
 
-class Main {
-    public static void main(String[] args){
-        		Scanner sc = new Scanner(System.in);
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		int input1 = sc.nextInt();
 		
 		String input2 = sc.next();
 		
-		long hashOut = 0;
+		BigInteger hashOut = new BigInteger("0");
 		
 		//input1번 반복
 		for(int i=0; i<input1; i++ ) {
 			
-			long pow = (int)Math.pow(31, i);
-			hashOut += (input2.codePointAt(i)-96)*(pow);
+			BigInteger input2B = new BigInteger(Integer.toString((input2.codePointAt(i)-96)));
+			
+			BigInteger M = new BigInteger("1234567891");
+			
+			//pow는 31의 i거듭제곱인 수를 나타낸다.
+			BigInteger pow = (new BigInteger("31")).pow(i);
+			
+			hashOut = hashOut.add( (pow).multiply(input2B).mod(M) );
+			
+			
 			
 		}
 		
 		System.out.println( hashOut );
 		
-    }
+
+	}
+
 }
