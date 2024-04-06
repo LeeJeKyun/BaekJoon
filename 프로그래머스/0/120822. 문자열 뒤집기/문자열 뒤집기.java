f991f9c0b752cd1f0b08df3836407174ce238a5f
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2024-04-06 15:11:37 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,8 @@
 class Solution {
     public String solution(String my_string) {
         StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
         
-        for(int i=my_string.length()-1; i>=0; i--) {
-            sb.append(my_string.charAt(i));
-        }
-        
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
