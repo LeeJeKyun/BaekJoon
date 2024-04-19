5a5e8706bf165a343e84143cdefabf2eb3d0ec38
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2024-04-19 13:36:03 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.67 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int i = 0;
+        List<Integer> stkList = new ArrayList<>();
+        while(true) {
+            if(i==arr.length) break;
+            if(stkList.isEmpty()){
+                stkList.add(arr[i]);
+                i++;
+            } else if (stkList.get(stkList.size()-1)<arr[i]) {
+                stkList.add(arr[i]);
+                i++;
+            } else if (stkList.get(stkList.size()-1) >= arr[i]){
+                stkList.remove(stkList.size()-1);
+            }
+        }
+        int[] stk = new int[stkList.size()];
+        for(int j=0; j<stk.length; j++) {
+            stk[j] = stkList.get(j);
+        }
+        
+        return stk;
+    }
+}
