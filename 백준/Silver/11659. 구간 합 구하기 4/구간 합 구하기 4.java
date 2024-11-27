a7975ee1f84c30dab5cc17d759f034bc7f6ded70
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2024-11-27 18:28:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1140 ms, Memory: 60272 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,36 +1,28 @@
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int length = sc.nextInt();
-        int count = sc.nextInt();
-        int[] iArr = new int[length];
-        for(int i=0; i<length; i++) {
-            iArr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int length = Integer.parseInt(st.nextToken());
+        int count = Integer.parseInt(st.nextToken());
+        long[] sArr = new long[length + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=length; i++) {
+            sArr[i] = sArr[i-1] + Integer.parseInt(st.nextToken());
         }
 
-        int[] sArr = new int[length];
-        for(int i=0; i<length; i++) {
-            if(i==0) {
-                sArr[i] = iArr[i];
-            } else {
-                sArr[i] = sArr[i-1] + iArr[i];
-            }
+        for (int r=0; r<count; r++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            System.out.println(sArr[j] - sArr[i-1]);
         }
-
-        int[] answer = new int[count];
-
-        for(int i=0; i<count; i++) {
-            int j = sc.nextInt() - 1;
-            int k = sc.nextInt() - 1;
-            if(j<=0) answer[i] = sArr[k];
-            else answer[i] = sArr[k] - sArr[j-1];
-        }
-        for(int a : answer) {
-            System.out.println(a);
-        }
-
     }
 }
