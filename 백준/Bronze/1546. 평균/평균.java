b42d5ecd55dd22c525b19069a3153afccd68539a
LeeJeKyun <121838709+LeeJeKyun@users.noreply.github.com>
2024-11-27 17:42:02 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 19096 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 class Main{
-    public static void main(String[] args){
+public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int subNum = sc.nextInt();    
-        int[] subScore = new int[subNum];
-        int total = 0;
-        
-        for(int i=0; i<subNum; i++){
-            subScore[i] = sc.nextInt();
+        int num = sc.nextInt();
+        int maxNum = 0;
+        int[] scoreArr = new int[num];
+        for(int i=0; i<num; i++) {
+            scoreArr[i] = sc.nextInt();
+            if(scoreArr[i] > maxNum) {
+                maxNum = scoreArr[i];
+            }
         }
-        Arrays.sort(subScore);
-        
-        for(int i=0; i<subNum; i++){
-            total += subScore[i];
+
+        double sum = 0;
+        for(int i=0; i<num; i++) {
+            sum += (scoreArr[i]/(double)maxNum)*100;
         }
-        
-        System.out.print((double)total/(subScore[subNum-1])*100/subNum);
-      
-        
+        System.out.println(sum/num);
     }
 }
